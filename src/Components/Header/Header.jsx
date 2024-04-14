443a28cdba0d156857f654a0c9289e25fb679f95
Pankaj Kumar Verma <pankajpatel916174@gmail.com>
2024-04-14 13:49:28 +0530

    header changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
    <div className='container header__container'>
       <h5>Hello ! I'M</h5>
       <h1>Pankaj Kumar Verma</h1>
-      <h5 className='text-light'>Junior Software Engineer @Transcend</h5>
+      <h5 className='text-light'>Junior Software Engineer @Transcend infosystem pvt ltd.</h5>
       <OTF/>
      <HeaderSocial/>
      <div className="me">
       <img src={ME} alt="me" />
      </div>
-     <a href='#contact'className="Scroll__Down">Scroll Down</a>
+     <a href='#contact'className="Scroll__Down" rel="noreferrer">Scroll Down</a>
     </div>
    </header>
    </>
